9e26decb0c89ed04a2d6598da479b6408a9ce8b0
724211471 <724211471@qq.com>
2020-06-19 18:51:42 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+let d=document.getElementById('clock')
+let ctx = d.getContext('2d')
+
+console.log(ctx)
+console.log('sss')
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer:{
+    contentBase: './dist'
+  },
+  plugins:[
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({title:'nnn'})
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
